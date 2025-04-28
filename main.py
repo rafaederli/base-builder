e2dbe8e75dd989f932f8542c4311f49f2b424796
Rafael Ederli <rafaederli@gmail.com>
2025-04-28 17:41:06 -0300

    feat: add save_list, type hints, export dataframe
    - Create save_list function to save missing pairs to a .txt file
    - Add type hints to function parameters for better code clarity
    - Export dataframe to CSV file for later use
    - Write specific instruction based on the provided field

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
 import pandas as pd
 
+def save_list(collection:list, field:str, path:str):
+    """
+    Saves a list of items to a .txt file in a subfolder.
+
+    Parameters:
+    collection (list): The list of items to be saved to the file.
+    field (str): The name of the field related to the list of items.
+    path (str): The full path to the .txt file.
+    
+    Returns:
+    The function creates a .txt file at the specified path and writes each item from the list as a new line in the file.
+    """
+    # Open the file in write mode
+    with open(path, "w") as file:
+    
+        # Write specific instructions at the beginning of the file
+        # depending on the field being processed
+        if field == "category_2":
+            file.write("ATTENTION: Inside 'maps.xlsx', in the 'category_1_category_2' sheet: add the missing category_1-category_2 pair and its corresponding category_2_model\n\n")
+        if field == "category_3":
+            file.write("ATTENTION: Inside 'maps.xlsx', in the 'category_1_category_3' sheet: add the missing category_1-category_3 pair and its corresponding category_3_model\n\n")
+
+        # Write only the elements that contain "PAIR NOT FOUND" to the file
+        for element in collection:
+            if "PAIR NOT FOUND" in element:
+                file.write(f"{element}\n")
+
 # Load 'sales.txt' file into a DataFrame
 df = pd.read_csv("sales.txt")
 
@@ -9,7 +36,7 @@ df["date"] = pd.to_datetime({"year": df["year"], "month": df["month"], "day": df
 # Set a 'date' column as the DateFrame index
 df.set_index(df["date"], inplace=True)
 
-def normalize_category_1(row):
+def normalize_category_1(row:pd.Series):
     """
     Update the value of the 'category_1' column based on the value of the 'category_0' column.
     If 'category_0' is 'cat_1', then 'category_1' will be updated to 'cat_1'.
@@ -45,7 +72,7 @@ category_3_mapping_dataFrame = pd.read_excel("maps.xlsx", sheet_name="category_1
 category_2_mapping_dictionary = {(row["category_1"], row["category_2"]): row["new_category_2"] for _, row in category_2_mapping_dataFrame.iterrows()}
 category_3_mapping_dictionary = {(row["category_1"], row["category_3"]): row["new_category_3"] for _, row in category_3_mapping_dataFrame.iterrows()}
 
-def normalize(row, column, dictionary):
+def normalize(row:pd.Series, column:str, dictionary:dict):
     """
     Updates the value of the specified column based on a mapping file.
 
@@ -64,3 +91,8 @@ def normalize(row, column, dictionary):
 # Apply the 'normalize' function to update the 'category_2' and 'category_3' columns with the mappings
 df["category_2"] = df.apply(lambda row: normalize(row, "category_2", category_2_mapping_dictionary), axis=1)
 df["category_3"] = df.apply(lambda row: normalize(row, "category_3", category_3_mapping_dictionary), axis=1)
+
+save_list(df["category_2"].unique(), "category_2", "maps/category_1-category_2.txt")
+save_list(df["category_3"].unique(), "category_3", "maps/category_1-category_3.txt")
+
+df.to_csv("sales2.txt")
